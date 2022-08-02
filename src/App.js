f08ceaf42e9b23d318ab5e04f95c323cddc536f6
Kahlil Hodgson <tartansandal@users.noreply.github.com>
2022-08-02 13:38:11 +1000

    Add the Urn to the main app for development

--- src/App.js
+++ src/App.js
@@ -3,14 +3,12 @@ import {
   ChakraProvider,
   Box,
   Text,
-  Link,
   VStack,
-  Code,
   Grid,
   theme,
 } from '@chakra-ui/react';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { Logo } from './Logo';
+import Urn from './components/Urn';
 
 function App() {
   return (
@@ -19,19 +17,10 @@ function App() {
         <Grid minH="100vh" p={3}>
           <ColorModeSwitcher justifySelf="flex-end" />
           <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
             <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
+              The Urn of Aristople
             </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
+            <Urn />
           </VStack>
         </Grid>
       </Box>
