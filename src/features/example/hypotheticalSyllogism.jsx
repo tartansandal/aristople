cc15439d23ed9cb85653ec0d255844d5b4e0bda5
Kahlil Hodgson <tartansandal@users.noreply.github.com>
2022-08-08 17:34:10 +1000

    Layout example using a table

--- src/features/example/hypotheticalSyllogism.jsx
+++ src/features/example/hypotheticalSyllogism.jsx
@@ -8,8 +8,39 @@ import {
   VStack,
   // SimpleGrid,
   // Divider,
+  Table,
+  Thead,
+  Tbody,
+  Tfoot,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+  TableContainer,
 } from '@chakra-ui/react';
 import { useSelector } from 'react-redux';
+
+const MImp = () => {
+  return (
+    <>
+      <Text fontSize="1.5rem" as="span">
+        &rarr;
+      </Text>
+      <sub>m</sub>
+    </>
+  );
+};
+
+const NImp = () => {
+  return (
+    <>
+      <Text fontSize="1.5rem" as="span">
+        &rarr;
+      </Text>
+      <sub>n</sub>
+    </>
+  );
+};
 
 const HypotheticalSyllogism = () => {
   const total = useSelector(state => state.urn.total);
@@ -22,32 +53,75 @@ const HypotheticalSyllogism = () => {
   return (
     <VStack spacing={50} alignItems="flex-start" justifyContent="space-between">
       <Heading>Hypothetical Syllogism</Heading>
-      
-      <Box fontStyle="italic">
-      <Heading size="md" mb={5}>Material Conditional</Heading>
-      <Text>
-        Pr(wood {'->'} large) = {(1 - ((wood - large_wood) / total)).toFixed(4)}
-      </Text>
-      <Text>
-        Pr(large {'->'} metal) = {(1 - (large - large_metal) / total).toFixed(4)}
-      </Text>
-      <Box borderColor='gray.600' borderTopWidth={2}></Box>
-      <Text>
-        Pr(wood {'->'} metal) = {(1 - (wood - 0) / total).toFixed(4)}
-      </Text>
+
+      <Box>
+        <Heading align="left" size="md" mb={5} color="blue.700">
+          Material Conditional
+        </Heading>
+        <TableContainer fontStyle="italic">
+          <Table variant="unstyled">
+            <Tbody>
+              <Tr>
+                <Td>
+                  Pr(wood <MImp /> large)
+                </Td>
+                <Td>=</Td>
+                <Td isNumeric>
+                  {(1 - (wood - large_wood) / total).toFixed(4)}
+                </Td>
+              </Tr>
+              <Tr>
+                <Td>
+                  Pr(large <MImp /> metal)
+                </Td>
+                <Td>=</Td>
+                <Td isNumeric>
+                  {(1 - (large - large_metal) / total).toFixed(4)}
+                </Td>
+              </Tr>
+              <Tr borderTopWidth={2} borderColor='gray.600'>
+                <Td>
+                  Pr(wood <MImp /> metal)
+                </Td>
+                <Td>=</Td>
+                <Td isNumeric>{(1 - (wood - 0) / total).toFixed(4)}</Td>
+              </Tr>
+            </Tbody>
+          </Table>
+        </TableContainer>
       </Box>
-      <Box fontStyle="italic">
-      <Heading size="md" mb={5}>Natural Conditional</Heading>
-      <Text>
-        Pr(wood {'->'} large) = {(large_wood / wood).toFixed(4)}
-      </Text>
-      <Text>
-        Pr(large {'->'} metal) = {(large_metal / large).toFixed(4)}
-      </Text>
-      <Box borderColor='gray.600' borderTopWidth={2}></Box>
-      <Text>
-        Pr(wood {'->'} metal) = {(0 / wood).toFixed(4)}
-      </Text>
+
+      <Box>
+        <Heading align="left" size="md" mb={5} color="blue.700">
+          Natural Conditional
+        </Heading>
+        <TableContainer fontStyle="italic">
+          <Table variant="unstyled">
+            <Tbody>
+              <Tr>
+                <Td>
+                  Pr(wood <NImp /> large)
+                </Td>
+                <Td>=</Td>
+                <Td isNumeric>{(large_wood / wood).toFixed(4)}</Td>
+              </Tr>
+              <Tr>
+                <Td>
+                  Pr(large <NImp /> metal)
+                </Td>
+                <Td>=</Td>
+                <Td isNumeric>{(large_metal / large).toFixed(4)}</Td>
+              </Tr>
+              <Tr borderTopWidth={2} borderColor='gray.600'>
+                <Td>
+                  Pr(wood <NImp /> metal)
+                </Td>
+                <Td>=</Td>
+                <Td isNumeric>{(0 / wood).toFixed(4)}</Td>
+              </Tr>
+            </Tbody>
+          </Table>
+        </TableContainer>
       </Box>
     </VStack>
   );
